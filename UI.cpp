b47cd9d02b73e1d24b68928af94bc804bcc50d6b
Chris Douris <christos.douris@gmail.com>
2022-06-30 19:29:15 +0300

    Added serialization code in the project. Issue : Invocation error

--- UI.cpp
+++ UI.cpp
@@ -1,4 +1,4 @@
-#include "Main.cpp"
+#include "serialization.h"
 #include <wx/wx.h>
 #include </Users/chrisdouris/Desktop/Dev_Projects/Libraries/C++/wxWidgets-3.1.6/macbuild/boost/boost/archive/binary_iarchive.hpp>
 #include </Users/chrisdouris/Desktop/Dev_Projects/Libraries/C++/wxWidgets-3.1.6/macbuild/boost/boost/archive/binary_oarchive.hpp>
@@ -17,15 +17,11 @@ class myFrame:public wxFrame{
 
 
  bool myApp::OnInit(){
-    mainClass mainFunction;
-    
-    std::string day = mainFunction.function();
-    std::string toDo = mainFunction.randomDo();
-    std::string toUndo = mainFunction.randomUndo();
+    startApp();
 
-    myFrame* frame = new myFrame(day, wxDefaultPosition, wxSize(800, 640));
-    wxStaticText* text = new wxStaticText(frame, wxID_ANY, toDo, wxDefaultPosition, wxDefaultSize, 0, "Do List");
-    wxStaticText* textTwo = new wxStaticText(frame, wxID_ANY, toUndo, wxPoint(-1,15), wxDefaultSize, 0, "Undo List");
+    myFrame* frame = new myFrame("Welcome", wxDefaultPosition, wxSize(800, 640));
+    wxStaticText* text = new wxStaticText(frame, wxID_ANY, "Hello", wxDefaultPosition, wxDefaultSize, 0, "Do List");
+    wxStaticText* textTwo = new wxStaticText(frame, wxID_ANY, "Hi", wxPoint(-1,15), wxDefaultSize, 0, "Undo List");
     frame->Show(true);
     return true;
 
---
+++ serial.cpp
@@ -0,0 +1,9 @@
+#include "Main.cpp"
+#include </Users/chrisdouris/Desktop/Dev_Projects/Libraries/C++/wxWidgets-3.1.6/macbuild/boost/boost/archive/binary_iarchive.hpp>
+#include </Users/chrisdouris/Desktop/Dev_Projects/Libraries/C++/wxWidgets-3.1.6/macbuild/boost/boost/archive/binary_oarchive.hpp>
+
+class serialization{
+    public:
+
+    mainClass
+}
